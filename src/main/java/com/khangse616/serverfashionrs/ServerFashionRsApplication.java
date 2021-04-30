eca2fcafd0e838c52c597a5aaa0fa91b2c6bb65e
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-04-30 17:38:06 +0700

    create project

---
+++ src/main/java/com/khangse616/serverfashionrs/ServerFashionRsApplication.java
@@ -0,0 +1,13 @@
+package com.khangse616.serverfashionrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerFashionRsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerFashionRsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/khangse616/serverfashionrs/ServerFashionRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khangse616.serverfashionrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerFashionRsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

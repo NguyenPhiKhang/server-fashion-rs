f86bc60a4b068ab8894d7f553df2bfe0aaef4962
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-05-03 06:17:59 +0700

    add image data model

--- src/main/java/com/khangse616/serverfashionrs/models/ImageData.java
+++ src/main/java/com/khangse616/serverfashionrs/models/ImageData.java
@@ -1,4 +1,90 @@
 package com.khangse616.serverfashionrs.models;
 
-public class ImageData {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "data_images")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class ImageData implements Serializable {
+    @Id
+    @JsonIgnore
+    private String id;
+    @Column(name = "name")
+    @JsonIgnore
+    private String name;
+    @Column(name = "type")
+    @JsonIgnore
+    private String type;
+    @Lob
+    @Column(name = "data")
+    @JsonIgnore
+    private byte[] data;
+
+    @Column(name="link")
+    private String link;
+
+
+//    @OneToOne(targetEntity = Product.class, mappedBy = "imgUrl", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+//    @JsonIgnore
+//    private Product product;
+
+    public ImageData(){}
+
+    public ImageData(String id, String name, String type, byte[] data) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.data = data;
+    }
+
+    public ImageData(String id, String name, String type, String link) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.link = link;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }

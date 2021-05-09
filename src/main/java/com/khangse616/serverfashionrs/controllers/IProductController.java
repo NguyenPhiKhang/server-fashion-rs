60cf208f3e2e707337571d52dd0ed7c6a9432624
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-05-10 01:55:41 +0700

    reconstruct interface product controller

--- src/main/java/com/khangse616/serverfashionrs/controllers/IProductController.java
+++ src/main/java/com/khangse616/serverfashionrs/controllers/IProductController.java
@@ -4,14 +4,21 @@ import com.khangse616.serverfashionrs.models.Product;
 import com.khangse616.serverfashionrs.models.dto.ProductDetailDTO;
 import com.khangse616.serverfashionrs.models.dto.ProductItemDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/default")
 public interface IProductController {
+    @GetMapping("/product/{id}")
     ResponseEntity<ProductDetailDTO> getProductById(@PathVariable int id);
+
+    @GetMapping("/cat/{idCategory}/products")
     ResponseEntity<List<ProductItemDTO>> getProductsByCategory(@PathVariable("idCategory") int idCategory, @RequestParam("filter") String filter, @RequestParam("p") int page);
+
+    @PostMapping("/product/generate-id-rating-star")
     ResponseEntity<String> generateIdRatingStarForProduct();
+
+    @GetMapping("/recommend/top-rating/{userId}")
     ResponseEntity<List<ProductItemDTO>> getProductTopRating(@PathVariable("userId") int userId, @RequestParam(value = "p", defaultValue = "1") int page);
 }
--- src/main/java/com/khangse616/serverfashionrs/controllers/impl/ProductController.java
+++ src/main/java/com/khangse616/serverfashionrs/controllers/impl/ProductController.java
@@ -3,7 +3,6 @@ package com.khangse616.serverfashionrs.controllers.impl;
 import com.khangse616.serverfashionrs.controllers.IProductController;
 import com.khangse616.serverfashionrs.mappers.impl.ProductDetailDTOMapper;
 import com.khangse616.serverfashionrs.mappers.impl.ProductItemDTOMapper;
-//import com.khangse616.serverfashionrs.mappers.impl.RatingRSDTOMapper;
 import com.khangse616.serverfashionrs.mappers.impl.RatingRSDTOMapper;
 import com.khangse616.serverfashionrs.models.Category;
 import com.khangse616.serverfashionrs.models.CosineSimilarity;
@@ -13,8 +12,6 @@ import com.khangse616.serverfashionrs.models.dto.ProductDetailDTO;
 import com.khangse616.serverfashionrs.models.dto.ProductItemDTO;
 import com.khangse616.serverfashionrs.models.dto.RecommendSystem.AVGRatedProductDTO;
 import com.khangse616.serverfashionrs.models.dto.RecommendSystem.RatingRSDTO;
-import com.khangse616.serverfashionrs.repositories.CategoryRepository;
-import com.khangse616.serverfashionrs.repositories.ProductRepository;
 import com.khangse616.serverfashionrs.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,7 +20,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.Timestamp;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -48,64 +44,57 @@ public class ProductController implements IProductController {
     @Autowired
     private IRecommendRatingService recommendRatingService;
 
-    @GetMapping("/product/{id}")
     @Override
-    public ResponseEntity<ProductDetailDTO> getProductById(@PathVariable int id) {
+    public ResponseEntity<ProductDetailDTO> getProductById(int id) {
         ProductDetailDTO productDetailDTO = new ProductDetailDTOMapper().mapRow(productService.findProductById(id), imageDataService);
         return ResponseEntity.ok().body(productDetailDTO);
     }
 
-    @GetMapping("/cat/{idCategory}/products")
     @Override
-    public ResponseEntity<List<ProductItemDTO>> getProductsByCategory(@PathVariable("idCategory") int idCategory, @RequestParam(defaultValue = "popular",value = "filter") String filter, @RequestParam("p") int page) {
-        Set<Category> list =  categoryService.findCategoriesByParentId(idCategory);
+    public ResponseEntity<List<ProductItemDTO>> getProductsByCategory(int idCategory, String filter, int page) {
+        Set<Category> list = categoryService.findCategoriesByParentId(idCategory);
 
         List<Integer> listId = new ArrayList<>();
         getListIdCategory(list, listId);
 
-        Pageable pageable = PageRequest.of(page-1, 20);
+        Pageable pageable = PageRequest.of(page - 1, 20);
 
         List<ProductItemDTO> listProduct = new ArrayList<>();
 
-        if(filter.equals("popular")) {
+        if (filter.equals("popular")) {
             Page<Product> pageProduct = productService.getProductsByCategoriesOrderByPopular(listId, pageable);
             listProduct = pageProduct.getContent().stream()
                     .map(value -> new ProductItemDTOMapper().mapRow(value, imageDataService)).collect(Collectors.toList());
-        }else{
-            if(filter.equals("new")){
+        } else {
+            if (filter.equals("new")) {
                 Page<Product> pageProduct = productService.getProductsByCategoriesOrderByNew(listId, pageable);
                 listProduct = pageProduct.getContent().stream()
                         .map(value -> new ProductItemDTOMapper().mapRow(value, imageDataService)).collect(Collectors.toList());
             }
         }
 
-        return  ResponseEntity.ok().body(listProduct);
+        return ResponseEntity.ok().body(listProduct);
     }
 
     @Override
-    @PostMapping("/product/generate-id-rating-star")
     public ResponseEntity<String> generateIdRatingStarForProduct() {
         productService.generateIdRatingStar();
-
         return ResponseEntity.ok().body("done");
     }
 
     @Override
-    @GetMapping("/recommend/top-rating/{userId}")
-    public ResponseEntity<List<ProductItemDTO>> getProductTopRating(@PathVariable("userId") int userId, @RequestParam(value = "p", defaultValue = "1") int page) {
+    public ResponseEntity<List<ProductItemDTO>> getProductTopRating(int userId, int page) {
         long startTime = new Date().getTime();
-        List<ProductItemDTO> list = new ArrayList<>();
+        List<ProductItemDTO> list;
 
         if (userId == 0 || !(ratingService.checkUserIsRated(userId) > 0)) {
             list = productService.productTopRating((page - 1) * 10).stream().map(value -> new ProductItemDTOMapper().mapRow(value, imageDataService)).collect(Collectors.toList());
-        }
-        else {
+        } else {
             if (!recommendRatingService.checkExistUser(userId)) {
                 recommend_product_for_user(userId);
             }
             list = productService.productRecommendForUser(userId).stream().map(value -> new ProductItemDTOMapper().mapRow(value, imageDataService)).collect(Collectors.toList());
         }
-
 
         long endTime = new Date().getTime();
         long difference = endTime - startTime;
@@ -113,14 +102,14 @@ public class ProductController implements IProductController {
         return ResponseEntity.ok().body(list);
     }
 
-    private void getListIdCategory(Set<Category> list, List<Integer> listId){
-        if(list.size()==0)
+    private void getListIdCategory(Set<Category> list, List<Integer> listId) {
+        if (list.size() == 0)
             return;
-        list.forEach(c->{
+        list.forEach(c -> {
             Set<Category> listCAT = c.getCategories();
-            if(listCAT.size()>0){
+            if (listCAT.size() > 0) {
                 getListIdCategory(listCAT, listId);
-            }else{
+            } else {
                 listId.add(c.getId());
             }
         });
@@ -164,9 +153,8 @@ public class ProductController implements IProductController {
                 double norm_pd1 = calc_norm(user_rated_product1);
                 double norm_pd2 = calc_norm(user_rated_product2);
                 double norm_pd1_mul_norm_pd2 = norm_pd1 * norm_pd2;
-//
                 double cosineSimilarity = norm_pd1_mul_norm_pd2 != 0.0 ? pd1_dot_pd2 / norm_pd1_mul_norm_pd2 : -1;
-//
+
                 CosineSimilarity a = new CosineSimilarity(product_id1, product_id2, cosineSimilarity);
                 cosSimilarities.add(a);
                 System.out.println("i: " + i + " - j: " + j);
@@ -257,7 +245,7 @@ public class ProductController implements IProductController {
         return ResponseEntity.ok().body(new RecommendRating(user_id, listProductRS_Show.toString()));
     }
 
-//    @PostMapping("/product/{productId}/{userId}/rating")
+    //    @PostMapping("/product/{productId}/{userId}/rating")
 //    public ResponseEntity<Rating> ratingProduct(@RequestBody Rating rating, @PathVariable("productId") int product_id, @PathVariable("userId") int user_id) {
 //        Random rd = new Random();
 //        int idRating;
@@ -309,18 +297,18 @@ public class ProductController implements IProductController {
 //
 //        return ResponseEntity.ok().body(new_rating);
 //    }
-private List<RatingRSDTO> calcUserRatedProduct(List<RatingRSDTO> list, int pd) {
-    List<RatingRSDTO> a = new ArrayList<>();
-    int size_list = list.size();
-    for (int i = 0; i < size_list; i++) {
-        if (list.get(i).getProductId() == pd) {
-            a.add(list.get(i));
+    private List<RatingRSDTO> calcUserRatedProduct(List<RatingRSDTO> list, int pd) {
+        List<RatingRSDTO> a = new ArrayList<>();
+        int size_list = list.size();
+        for (int i = 0; i < size_list; i++) {
+            if (list.get(i).getProductId() == pd) {
+                a.add(list.get(i));
+            }
         }
-    }
 //        list.removeAll(a);
-    return a;
+        return a;
 //        return list.stream().filter(v -> v.getProductId() == pd).collect(Collectors.toList());
-}
+    }
 
     private double p1_dot_p2(List<RatingRSDTO> list1, List<RatingRSDTO> list2) {
         int size_list1 = list1.size();

550b9c9830f23876ab5638a6b5ca65c0b3f70676
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-06-21 16:31:35 +0700

    add enviroment util

--- src/main/java/com/khangse616/serverfashionrs/mappers/impl/RatingDTOMapper.java
+++ src/main/java/com/khangse616/serverfashionrs/mappers/impl/RatingDTOMapper.java
@@ -1,5 +1,6 @@
 package com.khangse616.serverfashionrs.mappers.impl;
 
+import com.khangse616.serverfashionrs.Utils.EnvUtil;
 import com.khangse616.serverfashionrs.Utils.StringUtil;
 import com.khangse616.serverfashionrs.mappers.RowMapper;
 import com.khangse616.serverfashionrs.models.ImageData;
@@ -7,14 +8,16 @@ import com.khangse616.serverfashionrs.models.Rating;
 import com.khangse616.serverfashionrs.models.User;
 import com.khangse616.serverfashionrs.models.dto.RatingDTO;
 import com.khangse616.serverfashionrs.services.IImageDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.sql.Timestamp;
 import java.util.stream.Collectors;
 
 public class RatingDTOMapper implements RowMapper<RatingDTO, Rating> {
+
     @Override
     public RatingDTO mapRow(Rating rating) {
-        try{
+        try {
             RatingDTO ratingDTO = new RatingDTO();
             ratingDTO.setId(rating.getId());
             ratingDTO.setComment(rating.getComment());
@@ -22,24 +25,26 @@ public class RatingDTOMapper implements RowMapper<RatingDTO, Rating> {
             ratingDTO.setUserId(user.getId());
 //            ratingDTO.setImage( ImageUtil.addressImage(user.getImageAvatar().getId()));
             ratingDTO.setStar(rating.getStar());
-            ratingDTO.setImageAvatar(user.getImageAvatar()==null?"https://media3.scdn.vn/images/apps/icon_user_default.png":user.getImageAvatar().getLink());
+            ratingDTO.setImageAvatar(user.getImageAvatar() == null ? "https://media3.scdn.vn/images/apps/icon_user_default.png" : user.getImageAvatar().getLink());
             ratingDTO.setTimeUpdated(StringUtil.convertTimestampToString(rating.getTimeUpdated()));
             ratingDTO.setUserName(user.getName());
 
-            ratingDTO.setImageRating(rating.getDataImages().stream().map(v->"http:"+ v.getLink().replace("fill_size", "255x298")).collect(Collectors.toList()));
+//            ratingDTO.setImageRating(rating.getDataImages().stream().map(v->"http:"+ v.getLink().replace("fill_size", "255x298")).collect(Collectors.toList()));
 
-            rating.getProductAttribute().getOptionProductVarchars().forEach(v->{
-                if(v.getAttribute().getId()==80){
+            System.out.println(EnvUtil.getInstance().getServerUrlPrefix());
+
+            rating.getProductAttribute().getOptionProductVarchars().forEach(v -> {
+                if (v.getAttribute().getId() == 80) {
                     ratingDTO.setColor(v.getValue());
-                }else{
-                    if(v.getAttribute().getId()==164){
+                } else {
+                    if (v.getAttribute().getId() == 164) {
                         ratingDTO.setSize(v.getValue());
                     }
                 }
             });
 
             return ratingDTO;
-        }catch (Exception ex){
+        } catch (Exception ex) {
             return null;
         }
     }
